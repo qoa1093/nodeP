5624e6e9ff5cb3bea2007e71d4c6cc45bf84d9ee
qoa1093 <qoa1093@gmail.com>
2023-10-31 18:48:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ app.listen(port, function(){
     console.log("t시작");
 })
 
-//http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire?serviceKey=ibNDydv%2BVD1XxdnbSBCwRIZ4nfiYnptFv6FveU0wGfu4yRPisWSg%2F1gRJgdgRxOvOm6DlzNjTEJrjZkDyxNwbw%3D%3D&Q0=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C&Q1=%EA%B0%95%EB%82%A8%EA%B5%AC&QT=1&QN=%EC%82%BC%EC%84%B1%EC%95%BD%EA%B5%AD&ORD=NAME&pageNo=1&numOfRows=10
 
-app.get("/pharmach_list", (req, res)=>{
+
+app.get("/pharmach_list",(req, res)=>{
     let api = async() => {
         let response = null;
         try{
+            const { serviceKey } = await import('./service.js');
             response = await axios.get("http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire",{
                 params:{
-                    "serviceKey" : "ibNDydv+VD1XxdnbSBCwRIZ4nfiYnptFv6FveU0wGfu4yRPisWSg/1gRJgdgRxOvOm6DlzNjTEJrjZkDyxNwbw==",
+                    "serviceKey" : serviceKey,
                     "Q0":req.query.Q0,
                     "Q1":req.query.Q1,
                     "QT":req.query.QT,
